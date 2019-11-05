3b25db1ae5212b9629ad26a38e3cf80094b8c6ee
Marta <martacorcolesvalle@gmail.com>
2019-11-05 16:45:24 +0100

    coche funcionando

--- SPDVI/Create with Code/Prototype 1/Assets/Scripts/PlayerController.cs
+++ SPDVI/Create with Code/Prototype 1/Assets/Scripts/PlayerController.cs
@@ -4,16 +4,26 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private float speed = 5.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float forwardInput;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
 
+    }
     // Update is called once per frame
     void Update()
     {
-        // Move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        //Move the vehicle to the right (D) or to the left (A)
+        horizontalInput = Input.GetAxis("Horizontal");
+        //Move the vehicle forward (W) or backward (S)
+        forwardInput = Input.GetAxis("Vertical");
+        // Move the vehicle forward or backward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // Move the vehicle to the right or to the left
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
