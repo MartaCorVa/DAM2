1cb971d46d7c360b15a43379940cc01063b081b1
Marta <martacorcolesvalle@gmail.com>
2019-11-12 18:30:42 +0100

    WisdomGarden añadido

---
+++ SPDVI/WisdomGarden/Assets/Scripts/BookTouchByPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BookTouchByPlayer : MonoBehaviour
+{
+    private GameObject player;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+        if (collision.gameObject.name == player.name)
+        {
+            this.transform.localScale = new Vector3(gameObject.transform.localScale.x / 2, gameObject.transform.localScale.y / 2, gameObject.transform.localScale.z / 2);
+        }
+    }
+}
